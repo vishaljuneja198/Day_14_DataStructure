ecdabd1e59302cefefd12e87ceca105f8aee79c9
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-25 23:44:19 +0530

    uc1 linked lisit created

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure and Algorithm program ");
+
+        LinkedList l1 = new LinkedList();
+        l1.push(56);
+        l1.push(30);
+        l1.push(70);
+
+    }
+}
