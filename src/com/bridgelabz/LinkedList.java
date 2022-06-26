f2767d2a2d14347fb5c1d3fcecb7c2ab95f2b87e
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 11:47:45 +0530

    uc4 insertion of node

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -4,13 +4,15 @@ public class LinkedList<K> {
 
     Node<K> head, tail;
 
-    public void push(K key) {
+   /* public void push(K key) {
         Node<K> newNode = new Node<>(key);
         if (head != null) {
             newNode.next = head;
         }
         head = newNode;
     }
+
+    */
 
     public void print() {
         Node<K> temp = head;
@@ -28,6 +30,30 @@ public class LinkedList<K> {
             tail.next = newNode;
         }
         tail = newNode;
+    }
+
+
+
+
+    public Node<K> search(K key) {
+        Node<K> temp = head;
+        Node<K> foundKey = null;
+
+        while (temp != null) {
+            if (temp.key == key) {
+                foundKey = temp;
+                break;
+            }
+            temp = temp.next;
+        }
+        return foundKey;
+    }
+
+    public void insert(K key, K afterKey) {
+        Node<K> searchedNode = search(afterKey);
+        Node<K> insertValue = new Node<>(key);
+        insertValue.next = searchedNode.next;
+        searchedNode.next = insertValue;
     }
 
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -10,7 +10,7 @@ public class LinkedListMain {
         l1.push(56);
 
         l1.print();
-*/
+
         LinkedList l2 = new LinkedList();
         l2.add(56);
         l2.add(30);
@@ -18,6 +18,18 @@ public class LinkedListMain {
 
         System.out.println();
         l2.print();
+
+
+       */
+        LinkedList l3 = new LinkedList();
+        l3.add(56);
+        l3.add(30);
+
+
+
+        l3.insert(70,56);
+        System.out.println();
+        l3.print();
 
 
     }
